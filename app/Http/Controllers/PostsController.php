90723d9395578459b8237e5b0b77441a8bcf4d61
Jia Yi Lai <anniuu7134@gmail.com>
2017-11-21 17:08:55 +0800

    建立路由&PostsController(修正版)

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -13,27 +13,27 @@ class PostsController extends Controller
       */
   public function index()
    {
-        //
+
         return view('index');
     }
 
 
    public function show()
      {
-            //
+
            return view('post');
     }
 
 
     public function about()
     {
-           //
+
            return view('about');
     }
 
      public function contact()
      {
-           //
+
            return view('contact');
      }
 }
